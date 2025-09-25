0c63c610cfac9520327c8bbfad6488aabe8e661e
Vijayalaxmi Shankargouda Biradar <vijayalaxmibiradar567@gmail.com>
2025-09-25 22:31:23 +0530

    Update file.c

--- file.c
+++ file.c
@@ -3,50 +3,48 @@
 #include "contact.h"  
 #include "file.h"     
 
-
-
+// Function to save contacts to file
 void saveContactsToFile(AddressBook *addressBook) {
     
-     FILE *fptr ;
+    FILE *fptr ;  // File pointer
 
-     fptr= fopen("addressbook_file.csv","w");
+    fptr = fopen("addressbook_file.csv","w"); // Open file in write mode
      
-     //store count
-     fprintf(fptr,"%d\n",addressBook->contactCount);
+    // Store total contact count at the top
+    fprintf(fptr,"%d\n",addressBook->contactCount);
 
-     //write contact to file
-     for(int i=0;i<addressBook->contactCount;i++)
-     {
+    // Write each contact’s details (name, phone, email)
+    for(int i=0; i < addressBook->contactCount; i++)
+    {
         fprintf(fptr,"%s,%s,%s\n",
-        addressBook->contacts[i].name,
-        addressBook->contacts[i].phone,
-        addressBook->contacts[i].email);
-     }
-     fclose(fptr);
+            addressBook->contacts[i].name,
+            addressBook->contacts[i].phone,
+            addressBook->contacts[i].email);
     }
+    fclose(fptr); // Close the file
+}
 
+// Function to load contacts from file
 void loadContactsFromFile(AddressBook *addressBook)
 {
-    FILE *fptr = fopen("addressbook_file.csv","r");
+    FILE *fptr = fopen("addressbook_file.csv","r"); // Open file in read mode
 
-    if(fptr==NULL)
+    if(fptr == NULL) // If file not found, set contact count = 0
     {
-        addressBook->contactCount=0;
+        addressBook->contactCount = 0;
         return;
     }
     
-
-    //read contactcount
+    // Read contact count from file
     fscanf(fptr,"%d\n",&addressBook->contactCount);
     
-    //read contact
-    for(int i=0;i<addressBook->contactCount;i++)
+    // Read each contact’s details
+    for(int i = 0; i < addressBook->contactCount; i++)
     {
         fscanf(fptr,"%[^,],%[^,],%[^\n]\n",
-    addressBook->contacts[i].name,
-    addressBook->contacts[i].phone,
-    addressBook->contacts[i].email);
+            addressBook->contacts[i].name,
+            addressBook->contacts[i].phone,
+            addressBook->contacts[i].email);
     }
-    fclose(fptr);
-
+    fclose(fptr); // Close the file
 }
