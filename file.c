17b8088182b75202affed9000316cbe80cfc2c96
Vijayalaxmi Shankargouda Biradar <vijayalaxmibiradar567@gmail.com>
2025-09-25 22:30:17 +0530

    Update file.c

--- file.c
+++ file.c
@@ -36,7 +36,7 @@ void loadContactsFromFile(AddressBook *addressBook)
     }
     
 
-    //read count2qqqqqqqqwq
+    //read contactcount
     fscanf(fptr,"%d\n",&addressBook->contactCount);
     
     //read contact
@@ -48,4 +48,5 @@ void loadContactsFromFile(AddressBook *addressBook)
     addressBook->contacts[i].email);
     }
     fclose(fptr);
+
 }
